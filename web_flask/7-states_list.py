dad3432bb4a3ac39392f03d7083c4b608d9284af
MoanBekele <100833225+moanbekele@users.noreply.github.com>
2023-05-07 22:38:35 +0300

    static

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
+"""Starts a flask app
+    listens to 0.0.0.0:5000
+    
 """
 from models import storage
 from flask import Flask
--- web_flask/README.md
+++ web_flask/README.md
@@ -1,2 +1,3 @@
-# Web Static
+# 0x04. AirBnB clone - web framework
 
+In this project, I am introduced to my firts web framework - Flask.
