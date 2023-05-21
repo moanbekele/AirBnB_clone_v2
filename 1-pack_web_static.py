fcc0546226b5b7c25d61d3096fa9cb8ae3e6ffd4
MoanBekele <100833225+moanbekele@users.noreply.github.com>
2023-05-21 19:23:42 +0300

    Listen on  port 5000

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-'''Fabric script to generate .tgz archive'''
-
+"""Generates a .tgz archive from the contents of the web_static folder."""
 from fabric.api import local
-from datetime import datetime
-
-from fabric.decorators import runs_once
+import time
 
 
-@runs_once
 def do_pack():
-    '''generates .tgz archive from the contents of the web_static folder'''
-    local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
-
-    if result.failed:
+    """Generate an tgz archive from web_static folder"""
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(time.
+                                                    strftime("%Y%m%d%H%M%S")))
+    except:
         return None
-    return path
