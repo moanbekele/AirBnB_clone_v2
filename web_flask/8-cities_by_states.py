b489ff581e3e2cf5835f1569595628ecd7988c24
MoanBekele <100833225+moanbekele@users.noreply.github.com>
2023-05-21 19:47:54 +0300

    Expose States

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,29 +2,29 @@
 """Starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
 """
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
-
+from models.state import State
 app = Flask(__name__)
-
-
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def closedb(exc):
+    """ to close a database session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/cities_by_states')
+def states_list():
+    """ /states_list route """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    storage.reload()
+    app.run("0.0.0.0", 5000)
